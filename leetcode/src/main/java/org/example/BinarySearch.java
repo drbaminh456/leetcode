e2895019f0d96c105d65273675920ba05a02f1c0
Minh Dang <minh.ba.dang@mediastep.com>
2025-02-18 13:51:44 +0700

    x

---
+++ leetcode/src/main/java/org/example/BinarySearch.java
@@ -0,0 +1,89 @@
+package org.example;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 2, 3};
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        return new int[]{searchRangeLeft(nums, target), searchRangeRight(nums, target)};
+    }
+
+    public static int searchRangeLeft(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            if (nums[mid] == target) {
+                first = mid;
+            }
+        }
+        return first;
+    }
+
+    public static int searchRangeRight(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if (nums[mid] == target) {
+                first = mid;
+            }
+        }
+        return first;
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
--- leetcode/src/main/java/org/example/Main.java
+++ leetcode/src/main/java/org/example/Main.java
@@ -434,51 +434,6 @@ public class Main {
         return right - left - 1;
     }
 
-
-    public static int binarySearch(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            }
-        }
-        return -1;
-    }
-
-    public static int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
-        }
-
-        return -1;
-    }
-
     public static class ListNode {
         int val;
         ListNode next;
